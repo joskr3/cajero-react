e890cddd3acbc9a2d8dbd195d8a3a5ed200193bb
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-03-12 13:11:08 -0500

    trabajando con el estado y json-server

--- src/Vistas/Login/Login.tsx
+++ src/Vistas/Login/Login.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import "./styles.scss"
 import { arrayUsuarios } from '../../db';
 import { useRecoilState } from 'recoil';
 import { currentUserAtom } from '../../Atoms/User/UserAtom';
 import { IUser } from '../../Interfaces/IUser';
 import { useNavigate } from 'react-router-dom';
+import useFetch from "react-fetch-hook";
 
 function Login() {
 
@@ -13,9 +14,25 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useRecoilState<IUser | null>(currentUserAtom);
+  const [users, setUsers] = useState<IUser[] | null>(null);
+  //const { isLoading, data as I } = useFetch("http://localhost:3004/users")
+  ;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3004/users")
+      const data = await response.json()
+      setUsers(data)
+    }
+    fetchData()
+
+  }, [])
 
   function iniciarSesion() {
-    const userFound = arrayUsuarios.find(user => user.email === email && user.password === password);
+
+    // const userFound:IUser = data?.find((user:IUser) => user.email === email && user.password === password);
+    const userFound = users?.find((user: IUser) => user.email === email && user.password === password);
+
     setUser(userFound || null);
     user && navigate("/operaciones")
   }
